7a106200d783aea246959982f09a4f847eba6982
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-13 04:42:29 +0900

    fix: routes slug 수정

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,6 @@ import { ProductDetailPage } from "../pages/product-detail/ProductDetailPage";
 
 export const routes = [
   { path: "/", component: HomePage },
-  { path: "/product/:id", component: ProductDetailPage },
+  { path: "/product/:productId", component: ProductDetailPage },
   { path: "/*", component: NotFoundPage },
 ];
