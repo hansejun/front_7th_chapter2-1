a7f543ed9de288cbb2149baea6c714f49c44de7a
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-14 03:37:57 +0900

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => {
-  // GitHub Actions deploy 빌드에서만 서브패스 적용
-  const isGitHubPagesDeploy = process.env.GITHUB_ACTIONS && command === "build";
-
+export default defineConfig(({ mode }) => {
   return {
-    base: isGitHubPagesDeploy ? "/front_7th_chapter2-1/" : "/",
+    base: mode === "production" ? "/front_7th_chapter2-1/" : "/",
     build: {
       outDir: "dist",
     },
