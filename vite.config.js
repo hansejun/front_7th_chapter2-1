2e9d57a6eedb39425a858a4d82c7d4a10fb3e8be
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-14 02:38:11 +0900

    Revert "fix: 배포"
    This reverts commit 13c1dad9078a13ab7b544da8d9d530985447e0e3.

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-
-// GitHub Pages에 배포할 때 정적 자산 경로가 절대경로(/)로 나가지 않도록
-// 저장소 이름을 base로 명시합니다.
-export default defineConfig({
-  base: "/front_7th_chapter2-1/",
-});
