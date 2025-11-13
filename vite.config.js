f418bb7fb20315a28336b790dcb1a2430d9b2670
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-14 03:35:03 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig(({ command }) => {
   // GitHub Actions deploy 빌드에서만 서브패스 적용
@@ -9,12 +8,6 @@ export default defineConfig(({ command }) => {
     base: isGitHubPagesDeploy ? "/front_7th_chapter2-1/" : "/",
     build: {
       outDir: "dist",
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-          notFound: resolve(__dirname, "404.html"),
-        },
-      },
     },
   };
 });
